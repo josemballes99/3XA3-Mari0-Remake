bf7df29997debf2ecb75b4eb83f09c4b592dbd1f
Jose Miguel Ballesteros <josemballes99>
2016-11-16 09:23:33 -0500

    added some menus

---
+++ Mari0/src/Mari0/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public string startLevel;
+
+	public string settingsLevel;
+
+	public string helpLevel;
+
+	public string mainMenu;
+
+	public void NewGame(){
+		Application.LoadLevel (startLevel);
+	}
+
+	public void Settings(){
+		Application.LoadLevel (settingsLevel);
+	}
+
+	public void Help(){
+		Application.LoadLevel (helpLevel);
+	}
+
+	public void Home(){
+		Application.LoadLevel (mainMenu);
+	}
+}
---
+++ Mari0/src/Mari0/Assets/Scripts/Pause.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	public string mainMenu;
+
+	public GameObject pauseCanvas;
+
+	public bool isPaused;
+
+	public void Home(){
+		Application.LoadLevel (mainMenu);
+	}
+
+	public void Resume(){
+		isPaused = false;
+	}
+	void Start(){
+		isPaused = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (isPaused) {
+			pauseCanvas.SetActive (false);
+		} else {
+			pauseCanvas.SetActive (true);
+		}
+
+		if (Input.GetKeyDown (KeyCode.P)) {
+			isPaused = !isPaused;
+		}
+	}
+}
