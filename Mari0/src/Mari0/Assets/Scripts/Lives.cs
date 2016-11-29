46a6888047bdbb13c433aaab71649b9c95267893
jeff <pinedaj@mcmaster.ca>
2016-11-29 17:43:11 -0500

    Bug fix

---
+++ Mari0/src/Mari0/Assets/Scripts/Lives.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lives : MonoBehaviour {
+
+    private int numOfLives;
+
+
+
+    public int getNumOfLives()
+    {
+        return numOfLives;
+    }
+
+    public void hasDied()
+    {
+        numOfLives--;
+    }
+}
