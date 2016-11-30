956d13bba275158eeb19272a5a056222bf770fb8
David Hobson <davidhobson@Davids-MacBook-Pro-2.local>
2016-11-29 23:08:10 -0500

    Finished final level for final demo

---
+++ Mari0/src/Mari0/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	public float walkSpeed = 2.0f;
+	public float wallLeft = 22.0f;
+	public float wallRight = 48.0f;
+	float walkingDirection = 1.0f;
+	Vector3 walkAmount;
+
+	// Use this for initialization
+	void Start () {
+		Update();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
+		if (walkingDirection > 0.0f && transform.position.x >= wallRight){
+			walkingDirection = -1.0f;
+		}else if (walkingDirection < 0.0f && transform.position.x <= wallLeft){
+			walkingDirection = 1.0f;
+		}
+			
+		transform.Translate(walkAmount);
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D colisor){
+		if(colisor.gameObject.name.Equals("Mario") && colisor.gameObject.transform.position.y > 6.5){
+			Die();
+		}else{
+			colisor.gameObject.SetActive(false);
+		}
+	}
+
+	void Die(){
+		gameObject.SetActive(false);
+	}
+}
---
+++ Mari0/src/Mari0/Assets/Scripts/QuestionBlock.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestionBlock : MonoBehaviour {
+
+	private SpriteRenderer spriteRenderer;
+
+	public Sprite emptyBlock;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D colisor){
+		if(colisor.gameObject.name.Equals("Mario") && colisor.gameObject.transform.position.y < gameObject.transform.position.y){
+			spriteRenderer.sprite = emptyBlock;
+		}
+	}
+}
